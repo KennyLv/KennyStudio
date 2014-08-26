4e74d2963d17b07985c7915ca9f1c7bcf5c1b0b0
Qing, Lv <luqin@airbiquity.com>
2014-08-26 10:53:55 +0800

    update folder structure

---
+++ [REFER]/[Resume]/★ Jérôme Détraz - Designer multimédia ★_files/jquery.js
@@ -0,0 +1,292 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>江苏城市生活</title>
+<link href="css/reset.css" rel="stylesheet" type="text/css">
+<link href="css/index.css" rel="stylesheet" type="text/css">
+<script type="text/javascript" src="js/jquery1.42.min.js"></script>
+<script type="text/javascript" src="js/jquery.SuperSlide.2.1.js"></script>
+<script type="text/javascript" src="js/mobile.js"></script>
+<script type="text/javascript" src="js/ext.js"></script>
+<script type='text/javascript' src='http://js.adm.cnzz.net/js/abase.js'></script>
+
+
+<!-- 广告位：弹窗-->
+<script>CNZZ_SLOT_RENDER('285844');</script>
+
+</head>
+<body>
+<div class="ban-960"><a target="_blank" href="http://www.zhenhuasuan.com/"><img src="http://www.jscity.net/dns/images/ban-960.jpg"></a></div>
+
+<div class="top">
+
+<h3>您所请求的页面不存在或出现错误，您可尝试重新访问!
+<a target="_blank" href="http://www.jscity.net/">刷新试试看！</a>
+
+</h3>
+
+<div class="search-sitelogo">
+<a target="_blank" href="http://www.jscity.net/"><img src="images/logo.jpg"></a>	
+</div>
+
+<ul>
+
+<li class="menu-site">
+
+<a title='江苏城市生活网首页' target="_blank" href="http://www.jscity.net/" class="bg-gray">首页</a>
+
+<a title='唯品会' target="_blank" href="http://click.union.vip.com/redirect.php?url=eyJ1Y29kZSI6InRvcXlrOHhyIiwic2NoZW1lY29kZSI6ImQxMmhkaTVsIiwiY2hhbiI6Imt1cGluZzIifQ==">唯品会</a>
+<a title='1号店' target='_blank' href='http://pc.heima8.com/index.php?p=144654691&c=121604991&sc=[subcode]&durl=133858831&a=133512701&cc=138447962'>1号店</a>
+
+<a title='京东商城' target='_blank' href='http://pc.heima8.com/index.php?p=123558101&c=121604991&sc=[subcode]&durl=100002682&a=20059&cc=106038962'>京东商城</a>
+
+
+
+
+
+</li>
+<li class="menu-other">
+<a title='乐蜂网' target='_blank' href='http://pc.heima8.com/index.php?p=155679461&c=121604991&sc=[subcode]&durl=144127931&a=144067831&cc=144128102'>乐蜂网</a>
+<a title='当当商城' target='_blank' href='http://pc.heima8.com/index.php?p=123727941&c=121604991&sc=[subcode]&durl=100046601&a=20083&cc=100046682'>当当商城</a>
+<a title='苏宁易购' target='_blank' href='http://pc.heima8.com/index.php?p=155679911&c=121604991&sc=[subcode]&durl=150984861&a=150984631&cc=150984882'>苏宁易购</a>
+<a title='爱冲印' target='_blank' href='http://photo.189.cn/'>爱冲印</a>
+<a title='携程旅行网' target='_blank' href='http://pc.heima8.com/index.php?p=123727901&c=493&sc=[subcode]&durl=100075351&a=20143&cc=100004986'>携程旅行网</a>
+
+
+
+
+
+
+
+
+</li>
+
+
+</ul>
+
+
+ </div>
+<div class="header">
+
+
+
+<div class="search-ban">
+	<a target="_blank" href="http://www.063.com/yx/qs27/index.html?pid=300568"><img src="images/ban.jpg"></a>
+</div>
+
+<div class="search-box">
+	  <div class="search-logo">搜狗搜索</div>
+	  <div class="search">
+	  <form action="http://www.sogou.com/sogou" method="get" target="_blank">
+	  <input type="text" class="ipt" id="query" name="query"/>
+	  <input type="text" class="ipt" value="sogou-site-943aa0fcda4ee290" id="pid" name="pid" style=" display:none"/>
+	  <input class="btn" value="搜索" type="submit"/>
+	  </form>
+	  </div>
+</div>
+
+
+
+
+</div>
+
+
+
+
+<div class="con h320"> 
+  <!-- 主屏-->
+  <div class="zp">
+    <div class="hd">
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    <div class="bd">
+      <!--热点图文4条-->
+      <script src="http://202.102.110.211/d/js/js/1404267920.js"></script>
+
+      
+    </div>
+    <a class="prev" href="javascript:void(0)"></a> <a class="next" href="javascript:void(0)"></a> </div>
+  <!--新闻-->
+  <div class="news">
+  <!--热点新闻3条-->
+<script src="http://202.102.110.211/d/js/js/1404271256.js"></script>
+    <ul>
+      <!--精彩推荐5条-->
+       <script src="http://202.102.110.211/d/js/js/1404269250.js"></script>
+       
+    </ul>
+    <div class="more"><a target="_blank" href="http://www.jscity.net/"><img src="images/more.jpg" ></a></div>
+  </div>
+  <!--app-->
+  <div class="con-tab">
+    <div class="hd">
+					<ul>
+                    		<li><img src="images/t-hotPic.jpg" ></li>
+                  </ul>
+    </div>
+    <div class="bd-list">
+		<!--script src="http://news.jscity.net/jsjs/70.js"></script-->
+        <ul>
+        <li><a  target="_blank" href="http://news.jscity.net/pic/meinv/2014-08-04/249.html" ><img src="images/pic-01.jpg">《破产姐妹》吐槽女
+</a></li>
+        <li><a  target="_blank" href="http://news.jscity.net/pic/qiche/2014-08-04/246.html" ><img src="images/pic-02.jpg">奥迪(进口)-奥迪A8</a></li>
+        <li><a  target="_blank" href="http://news.jscity.net/tourism/2014-08-04/5257.html"><img src="images/pic-03.jpg">夏季养生吃三薯</a></li>
+        <li><a  target="_blank" href="http://news.jscity.net/tourism/2014-08-01/5024.html" ><img src="images/pic-04.jpg">枸杞岛浪漫之旅</a></li>
+        <li><a  target="_blank" href="http://news.jscity.net/pic/meinv/2014-08-04/248.html" ><img src="images/pic-05.jpg">怀念夏天的味道</a></li>
+        <li><a  target="_blank" href="http://union.netease.com/gs2/union/client/goto/?apid=7&s=9c1QXllXNPWXHnE9K9%2Fpif8WexY%3D&atid=0&cid=278&ctid=921&pid=6&cnid=3" ><img src="images/pic-06.jpg">天下3</a></li>
+      </ul> 
+      
+
+      
+      
+    </div>
+  </div>
+  <!--<div class="app-con"> APP 热门产品
+    <ul>
+      <li><a href="" ><img src="images/app.png">新浪新闻</a></li>
+    </ul>
+  </div>--> 
+</div>
+
+<div class="banner">
+<!-- 广告位：通栏1-->
+<script>CNZZ_SLOT_RENDER('285845');</script>
+</div>
+
+
+
+<div class="foot-link">
+
+    <span class="foot-01">
+        <a target="_blank" title="红枣" href="http://meal.taobao.com/item.htm?spm=686.1000925.1000774.45.b0byCo&id=38633833103">
+        <img src="http://img02.taobaocdn.com/imgextra/i2/688234950/TB2UzdMaXXXXXaqXpXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-02">
+        <a target="_blank" title="神农架原生态特产馆"  href="http://meal.taobao.com/item.htm?spm=a1z10.3.w4002-5502474744.39.u5zFRs&id=38056973377">
+        <img src="http://img01.taobaocdn.com/imgextra/i1/1951577510/TB2wIROaXXXXXcOXXXXXXXXXXXX_!!1951577510.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-03">
+        <a target="_blank" title="桂圆 农大夫"  href="http://item.taobao.com/item.htm?id=40071543657">
+        <img src="http://img02.taobaocdn.com/imgextra/i2/688234950/TB2tOBLaXXXXXbqXpXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-04">
+        <a target="_blank" title="果360旗舰店"  href="http://detail.tmall.com/item.htm?spm=a220z.1000880.0.0.BcJjmn&id=39380987735">
+        <img src="http://img01.taobaocdn.com/imgextra/i1/688234950/TB2noBKaXXXXXa4XpXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-05">
+        <a target="_blank" title="红薯"  href="http://item.taobao.com/item.htm?id=38864672763">
+        <img src="http://img03.taobaocdn.com/imgextra/i3/688234950/TB2rDBSaXXXXXaRXXXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-06">
+        <a target="_blank" title="菊花"  href="http://meal.taobao.com/item.htm?spm=a1z10.1.w4023-5445336589.4.q0Q9a7&id=36268653605">
+        <img src="http://img03.taobaocdn.com/imgextra/i3/688234950/TB2RGNSaXXXXXbxXXXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-07">
+        <a target="_blank" title="葛根粉"  href="http://item.taobao.com/item.htm?id=22397067330">
+        <img src="http://img01.taobaocdn.com/imgextra/i1/688234950/TB2EVXUaXXXXXXqXXXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-08">
+        <a target="_blank" title="尤溪红"  href="http://meal.taobao.com/item.htm?spm=686.1000925.1000774.24.hhel4y&id=37273059584">
+        <img src="http://img04.taobaocdn.com/imgextra/i4/688234950/TB23RVRaXXXXXbmXXXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-09">
+        <a target="_blank" title="陈皮普洱茶"  href="http://meal.taobao.com/item.htm?spm=a1z10.1.w6139801-5164622836.1.XgBPqU&id=24750144387">
+        <img src="http://img03.taobaocdn.com/imgextra/i3/688234950/TB2CelUaXXXXXaKXXXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+    
+    <span class="foot-10">
+        <a target="_blank" title="食在当季"  href="http://item.taobao.com/item.htm?id=38864672763">
+        <img src="http://img01.taobaocdn.com/imgextra/i1/688234950/TB2EadLaXXXXXX8XpXXXXXXXXXX_!!688234950.jpg">
+        </a>
+    </span>
+
+</div>
+
+
+
+
+
+<!--tab-->
+<div class="tab">
+  <div class="hd">
+    <h1>爱游戏</h1>
+    <div class="more"><iframe src="http://vnet.smartjs.cn/webdns.do" scrolling="no" frameborder="0" width="270" height="30"></iframe></iframe></div>
+    <ul>
+      <li>网页游戏</li>
+<!--      <li>安卓游戏</li>
+      <li>苹果游戏</li>-->
+    </ul>
+  </div>
+  <div class="bd">
+    <ul>
+      <li class="first">
+      <!-- 广告位：游戏多图-->
+<script>CNZZ_SLOT_RENDER('286340');</script>
+</li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_286_0" target="_blank"><img src="images/18096-1.jpg"><span>街机三国</span></a></li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_291_0" target="_blank"><img src="images/18096-2.jpg"><span>九龙朝</span></a></li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_288_0" target="_blank"><img src="game/2.jpg"><span>武尊</span></a></li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_292_0" target="_blank"><img src="game/3.jpg"><span>攻城掠地</span></a></li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_289_0" target="_blank"><img src="game/4.jpg"><span>武易</span></a></li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_287_0" target="_blank"><img src="game/5.jpg"><span>烈焰</span></a></li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_189_0" target="_blank"><img src="game/6.jpg"><span>三国演义</span></a></li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_195_0" target="_blank"><img src="game/7.jpg"><span>三国风云2</span></a></li>
+      <li><a href="http://webgameurl.com/pre.php?r=9021_172_0" target="_blank"><img src="game/8.jpg"><span>欧冠足球</span></a></li>
+
+    </ul>
+    <ul>
+      <li class="first"><a href="http://webgameurl.com/pre.php?r=9021_290_0" target="_blank"><img src="images/318308.jpg"><span>女神联盟</span></a></li>
+      <li><a href=""><img src="images/pic1.jpg"><span>标题标题</span></a></li>
+    </ul>
+    <ul>
+       <li class="first"><a href="http://webgameurl.com/pre.php?r=9021_290_0" target="_blank"><img src="images/318308.jpg"><span>女神联盟</span></a></li>
+      <li><a href=""><img src="images/pic1.jpg"><span>标题标题</span></a></li>
+    </ul>
+  </div>
+</div>
+<div class="banner">
+</div>
+<div class="footer" align="center">苏ICP备11053961号-6</div>
+</body>
+<script type="text/javascript">
+		jQuery(".zp").slide({mainCell:".bd ul",autoPlay:true});
+		jQuery(".con-tab").slide();
+		jQuery(".tab").slide();
+		</script>
+
+<!-- 广告位：对联-->
+<script>CNZZ_SLOT_RENDER('296704');</script>
+
+
+
+<!-- 广告位：富媒体-->
+<script>CNZZ_SLOT_RENDER('296698');</script>
+
+
+<!--
+流量统计代码-->
+<div style="display:none"><script src="http://s4.cnzz.com/z_stat.php?id=1000508659&web_id=1000508659" language="JavaScript"></script></div>
+</html>
---
+++ [REFER]/jQuery Tools vs jQuery UI - Marc Grabanski_files/widgets.js
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<HTML><HEAD><META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=iso-8859-1">
+<TITLE>ERROR: The requested URL could not be retrieved</TITLE>
+<STYLE type="text/css"><!--BODY{background-color:#ffffff;font-family:verdana,sans-serif}PRE{font-family:sans-serif}--></STYLE>
+</HEAD><BODY>
+<H1>ERROR</H1>
+<H2>The requested URL could not be retrieved</H2>
+<HR noshade size="1px">
+<P>
+While trying to retrieve the URL:
+<A HREF="http://platform.twitter.com/widgets.js">http://platform.twitter.com/widgets.js</A>
+<P>
+The following error was encountered:
+<UL>
+<LI>
+<STRONG>
+Connection to 59.24.3.173 Failed
+</STRONG>
+</UL>
+
+<P>
+The system returned:
+<PRE><I>    (110) Connection timed out</I></PRE>
+
+<P>
+The remote host or network may be down.  Please try the request again.
+<P>Your cache administrator is <A HREF="mailto:root">root</A>. 
+
+<BR clear="all">
+<HR noshade size="1px">
+<ADDRESS>
+Generated Tue, 12 Aug 2014 02:53:36 GMT by proxy.eobu.com (squid/2.6.STABLE21)
+</ADDRESS>
+</BODY></HTML>
