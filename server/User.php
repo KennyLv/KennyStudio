bd8524d97d64c8bd974cba7e4f31a6207818708f
Qing, Lv <luqin@airbiquity.com>
2014-08-28 17:44:57 +0800

    add rest server learn

---
+++ server/User.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once('restfulRequestRouter.php');
+
+    class User {
+		private $id;  
+		private $name;  
+		
+		public function __construct(){  
+			$this->id          = '';  
+			$this->name   = '';  
+		}  
+		
+        public function get() {
+            echo "get";
+        }
+		
+        public function getById($id) {
+            echo "getById";
+        }
+		
+        public function add() {
+            echo "add";
+        }
+		
+        public function update() {
+            echo "update";
+        }
+		
+		
+        public function delete($id) {
+            echo "delete";
+        }
+		
+		
+		
+    }
+	
+?>
--- server/index.php
+++ server/index.php
@@ -1,10 +1,16 @@
 <?php
+//ÔÊÐí¿çÓò·ÃÎÊ
+header("Access-Control-Allow-Origin: *");
+require_once('restfulRequestRouter.php');
 
+
+$r = new Router;
+$r->dispatch();
+/*
 $response = array(); 
 $response["error"] = true;
 $response["message"] = "Oops! An error occurred while loading";
 
-
 echo json_encode($response);
-
+*/
 ?>
