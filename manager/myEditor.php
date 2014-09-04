574119bc827e87af3722890d75856f32e3611bdb
Qing, Lv <luqin@airbiquity.com>
2014-09-04 17:29:23 +0800

    add UMeditor for backend

---
+++ manager/myEditor.php
@@ -0,0 +1,10 @@
+﻿<?php
+?>
+<link href="./umeditor1_2_2_utf8_php/themes/default/css/umeditor.css" type="text/css" rel="stylesheet">
+<script type="text/javascript" charset="utf-8" src="./umeditor1_2_2_utf8_php/third-party/jquery.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="./umeditor1_2_2_utf8_php/umeditor.config.js"></script>
+<script type="text/javascript" charset="utf-8" src="./umeditor1_2_2_utf8_php/umeditor.min.js"></script>
+<script type="text/javascript" charset="utf-8" src="./umeditor1_2_2_utf8_php/lang/zh-cn/zh-cn.js"></script>
+<script type="text/plain" id="myEditor" style="width:800px;min-height:300px;">
+		<p>这里我可以写一些输入提示</p>
+</script>
