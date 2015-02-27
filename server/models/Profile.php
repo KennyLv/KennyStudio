ec410efecf0171d7dd350638ba08247ec5463a4f
Qing, Lv <luqin@airbiquity.com>
2015-02-27 17:06:50 +0800

    update profile for resume

--- server/models/Profile.php
+++ server/models/Profile.php
@@ -19,28 +19,79 @@
 			switch($id) {
 				case "employ" : 
 					$resopnse = json_decode( '[
-					{"yearFrom": "2001.8~2009.9", "yearTo":"8888.8~9999.9","companyName":"Infoition","jobTitle":"Product Marketing Manager","description":"Planning and development included market research, competitor analysis, pricing strategies, messaging, prod"},
-						{"yearFrom": "", "yearTo":"","companyName":"","jobTitle":"","description":["1","2","3","4"]},
-						{"yearFrom": "", "yearTo":"","companyName":"","jobTitle":"","description":["","","",""]},
-						{"yearFrom": "", "yearTo":"","companyName":"","jobTitle":"","description":["","","",""]}
-					]');
+    {
+        "yearFrom": "2008.8~2010.8",
+        "yearTo": "",
+        "companyName": "Infoition",
+        "jobTitle": "Product Marketing Manager",
+        "description": "Planning and developmesdcfsddfvfdvdfgvdfgfdgdgdsgsdgdgdgergbgdvaserwefdfvcsdewrnt included market research, competitor analysis, pricing strategies, messaging, prod"
+    },
+    {
+        "yearFrom": "",
+        "yearTo": "",
+        "companyName": "",
+        "jobTitle": "",
+        "description": [
+            "1",
+            "2",
+            "3",
+            "4"
+        ]
+    },
+    {
+        "yearFrom": "",
+        "yearTo": "",
+        "companyName": "",
+        "jobTitle": "",
+        "description": [
+            "",
+            "",
+            "",
+            ""
+        ]
+    },
+    {
+        "yearFrom": "",
+        "yearTo": "",
+        "companyName": "",
+        "jobTitle": "",
+        "description": [
+            "",
+            "",
+            "",
+            ""
+        ]
+    }
+]');
 				break;
 				case "education" :
-					$resopnse = json_decode( '[	{
-						"yearFrom": "2004-2008", 
-						"yearTo":"2008",
-						"major":"Statistics",
-						"school":"School of Finance and Economics",
-						"university":"University of Jiangsu"
-					},
-					{
-						"yearFrom": "2008-2010", 
-						"yearTo":"2010",
-						"major":"Marketing & Advertising - B.A.",
-						"school":"School of Communications",
-						"university":"University of Central Florida"
-					}]');
+					$resopnse = json_decode( '[
+    {
+        "yearFrom": "2004-2008",
+        "yearTo": "2008",
+        "major": "Statistics",
+        "school": "School of Finance and Economics",
+        "university": "University of Jiangsu"
+    },
+    {
+        "yearFrom": "2008-2010",
+        "yearTo": "2010",
+        "major": "Marketing & Advertising - B.A.",
+        "school": "School of Communications",
+        "university": "University of Central Florida"
+    }
+]');
 				break;
+				
+				case "skills" :
+					$resopnse = json_decode( '{
+								"Ç°¶Ë¿ª·¢": {".NET":"ASP.NET","Javascript": ["Nodejs","AngualrJs","Backbone","Canjs","RequireJs","","",""], "css" : ["css3","LESS"], "HTML5":[]},
+								
+								"Mobile": ["", "", "", "", "", "", "", "", "", "", ""]
+						}');
+				break;
+				
+				
 				default:
 				$resopnse = "Data not found...";
 			
